b245ae762d77600b7528caae2a0f239691eeb9a6
maiaarrais <maiacarrais@gmail.com>
2025-01-16 14:29:22 -0500

    Task 1
    ellipse with different colors, shapes and positions

--- p5-rev-template/js/script.js
+++ p5-rev-template/js/script.js
@@ -4,8 +4,19 @@ function setup() {
   createCanvas(300, 300);
 
   background(0);
+  
+  let c1 = color(159, 43, 104)
 
-  ellipse(50, 50, 80, 80);
+  fill(c1);
+  ellipse(30, 30, 30);
+
+  let c2 = color(218, 112, 214);
+  fill(c2);
+  ellipse (60, 60, 40);
+
+  let c3 = color(255, 0, 255);
+  fill(c3);
+  ellipse (100, 100, 50);
 
 }
 
