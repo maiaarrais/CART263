c4d203d859ebd3cee8927e1ef95135e998e85a7b
maiaarrais <maiacarrais@gmail.com>
2025-01-18 18:20:28 -0500

    implementation of task3

--- p5-rev-template/js/task3.js
+++ p5-rev-template/js/task3.js
@@ -1,25 +1,66 @@
+x1 = 20;
+x2 = 40; 
+x3 = 40; 
+y1 = 20; 
+y2 = 40; 
+y3 = 300;
+w = 50;
+r1 = 218;
+r2 = 255;
+r3 = 199; 
+g1 = 247; 
+g2 = 195;
+g3 = 0;
+b1 = 166;
+b2 = 0;
+b3 = 57;
+
+
+
+
 function setup(){
     createCanvas(400, 400);
 
-    let x = random(width);
-    let y = random(height);
-    let w = random(10, 40);
-    let h = random(10, 40);
-    rectangle = new Rectangle(x, y, w, h);
 }
 
 function draw(){
-    background(0)
-    rectangle.show(mouseX, mouseY);
+    background(0);
+    fill(r1, g1, b1);
+    rect(x1, y1, w);
+    fill(r2, g2, b2);
+    rect(x2, y2, w);
+    movingSquare();
+    fill(r3, g3, b3);
+    rect(x3, y3, w);
     
 }
 
 function mouseClicked(){
-   
-    
+   x1 += 30 ;
+   if (x1 > width){
+    x1 = 20;
+   }
 
 }
 
-function mouseMoved(){
+function movingSquare(){
+    if (y3 <= width){
+        y3 += 5;
+        x3 = x1
+    }
+    else if (y3 >= width){
+        y3  = 20;
+    }
 
+}
+
+function keyPressed(){
+    if (keyCode === 32){
+        x2+=20;
+        y2+=15;
+    }
+    if (y2 >=height || x2 >=width){
+        y2 = 40;
+        x2 = 40;
+    }
 }
