4edad44404bd21aa82092f99ad0d89124cf3033b
maiaarrais <maiacarrais@gmail.com>
2025-01-30 15:04:43 -0500

    drawing circles based on mouse position on the canvas

--- week3/js/move.js
+++ week3/js/move.js
@@ -1,3 +1,36 @@
 window.onload = function(){
     console.log("move");
+
+    // we want to do something when the mouse is over the box :)
+let drawBox = document.querySelector("#draw-box-a");
+ 
+//A: add event listener + callback
+drawBox.addEventListener("mousemove", moveCallBack);
+ 
+function moveCallBack(e){
+    console.log("mouse move");
+    // B: note these are the same ... 
+    console.log(this);
+    console.log(e.target);
+    //C: get the mouse coords
+//    drawBox.innerHTML= `<p> x: ${e.clientX}, y:${e.clientY} </p>`;
+
+   let rect = this.getBoundingClientRect();
+    console.log(rect);
+   //relative to the WINDOW...
+   //DIFFERENCE TO ENSURE COORDS ARE RELATIVE
+   let offsetX = e.clientX-rect.x;
+   let offsetY = e.clientY-rect.y;
+//   drawBox.innerHTML+= `<p> offset_x: ${offsetX}, offset_y:${offsetY} </p>`;
+
+  let p = document.createElement("div");
+    p.classList.add("point");
+    p.style.left = offsetX+"px";
+    p.style.top = offsetY+"px";
+    this.appendChild(p);
+
 }
+
+
+}
+
